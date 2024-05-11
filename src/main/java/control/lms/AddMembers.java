7f123a52c23cb5e609ff6b27f544cbfe45f9c06c
Phuc <baophuc.itds.hcmiu@gmail.com>
2024-05-11 16:39:54 +0700

    Add "Add Members" feature

---
+++ src/main/java/control/lms/AddMembers.java
@@ -0,0 +1,34 @@
+package control.lms;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AddMembers {
+
+    public TextField addMemberName;
+    public TextField addMemberGender;
+    public TextField addMemberEmail;
+    public TextField addMemberPhoneNumber;
+    public TextField addMemberAddress;
+    public Button saveBtn;
+    public Button cleanBtn;
+
+    public void save(MouseEvent event) {
+    }
+
+    public void highlightSaveButton(MouseEvent event) {
+    }
+
+    public void resetSaveButtonColor(MouseEvent event) {
+    }
+
+    public void clean(MouseEvent event) {
+    }
+
+    public void highlightCleanButton(MouseEvent event) {
+    }
+
+    public void resetCleanButtonColor(MouseEvent event) {
+    }
+}
